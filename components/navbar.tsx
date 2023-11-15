a360cec8997e0c98ae81ba6fe1a21008f4d2822b
aviral <avi14320@gmail.com>
2023-11-15 07:10:54 +0530

    updated card slider

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,70 +1,137 @@
+"use client";
+import { useState, useEffect } from "react";
+
 import Image from "next/image";
 
 const navbar = () => {
+  const [isLarge, setIsLarge] = useState(false);
+
+  useEffect(() => {
+    // Run on initial render
+    setIsLarge(window.innerWidth > 968);
+
+    // Set up event listener
+    const handleResize = () => {
+      setIsLarge(window.innerWidth > 968);
+    };
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="w-full -mt-52 flex justify-between items-center">
-      <div className="relative flex items-center justify-center ml-14 ">
-        <div className="absolute -left-[68px] ">
+    <div className="w-full min-w-[100px] -mt-52 flex nav_sm:flex-row flex-col justify-between items-center">
+      {/* ********************************************************************* */}
+      {/* 1st component */}
+
+      <div className="relative flex items-center justify-center ml-8 ">
+        <div className="absolute -left-[30px] ">
           <Image
             src="/binance_logo.png"
-            width={120}
-            height={120}
+            width={isLarge ? 80 : 60}
+            height={isLarge ? 80 : 60}
             alt="Binance image"
           />
         </div>
-        <div className="flex items-center justify-center bg-[#ffffff] px-5 py-3 pl-[40px] rounded-full">
-          <div className="mr-[10px] font-extrabold text-[#29005c] text-2xl">
+        <div
+          className={`flex nav_lg:flex-row flex-col items-center justify-center bg-[#ffffff] ${
+            isLarge
+              ? "px-5 py-1 pl-[40px] rounded-full"
+              : "px-5 py-1 pl-[25px] rounded-lg rounded-br-3xl"
+          }`}
+        >
+          <div
+            className={` ${
+              isLarge ? "mr-[20px]" : ""
+            } font-extrabold text-[#29005c] nav_lg:text-xl text-sm`}
+          >
             BNBUSD
           </div>
-          <div className="text-[#29005c] font-medium">$228.5332</div>
+          <div className="text-[#29005c] font-medium text-xs">$228.5332</div>
         </div>
       </div>
-      <div className="hidden justify-between items-center px-[30px]  nav_cards:flex">
-        <div className="relative left-[137px]">
-          <Image src="nav_card.svg" width={140} height={140} alt="nav_cards" />
+      {/* ******===========*************===========********************* */}
+      {/* 2nd component */}
+      <div className="hidden justify-between items-center px-[30px]  nav_lg:flex">
+        <div className="relative left-[107px]">
+          <Image src="nav_card.svg" width={100} height={100} alt="nav_cards" />
         </div>
         <div className="w-full">
           <div className="bg-white  rounded-full gap-14 flex justify-between items-center py-1 px-1">
             <button>
               <Image
                 src="nav_arrow_left.svg"
-                width={35}
-                height={35}
+                width={25}
+                height={25}
                 alt="arrow"
               />
             </button>
             <button>
               <Image
                 src="nav_arrow_right.svg"
-                width={35}
-                height={35}
+                width={25}
+                height={25}
                 alt="arrow"
               />
             </button>
           </div>
         </div>
       </div>
-      <div className="w-[600px]">
-        <div className="flex p-5 items-center">
-          <div className="relative">
-            <div className="bg-white rounded-full flex justify-center items-center px-3 py-2 pr-[65px] gap-2 mr-10">
-              <div className="text-3xl text-[#7836d3] font-bold">00:38</div>
-              <div className="font-medium">5m</div>
+      {/* ******===========*************===========********************* */}
+      {/* 3rd component */}
+      <div className="w-full nav_sm:w-[250px] nav_lg:w-[390px] flex justify-center items-center">
+        <div className="flex nav_md:flex-row p-5 flex-row-reverse items-center">
+          <div className="relative nav_md:mr-3 -mr-1 ml-2">
+            <div className="bg-white rounded-lg rounded-bl-3xl nav_lg:rounded-full flex nav_lg:flex-row flex-col  justify-center items-center px-2 nav_lg:py-1 pr-[30px] nav_lg:pr-[65px] nav_lg:gap-2 mr-4">
+              <div className="nav_lg:text-lg text-sm text-[#7836d3] font-extrabold">
+                00:38
+              </div>
+              <div className="font-medium text-xs ">5m</div>
             </div>
             {/* right-7 top-[341px] */}
-            <div className="absolute  right-[10px] -top-[21px]">
-              <Image src="clock.svg" width={90} height={90} alt="clock" />
+            <div className="absolute  nav_lg:right-[5px] nav_lg:-top-[21px] -right-[5px] -top-[5px] ">
+              <Image
+                src="clock.svg"
+                width={isLarge ? 70 : 50}
+                height={isLarge ? 70 : 50}
+                // width={70}
+                // height={70}
+                alt="clock"
+              />
             </div>
           </div>
-          <div className="flex gap-[7px] mr-[50px]">
-            <div className="bg-[#7b6ba7] rounded-3xl p-4">
-              <Image src="question.svg" width={150} height={150} alt="ques" />
+          <div className="flex gap-[2px] nav_md:gap-[7px] mr-[1px] items-center justify-center">
+            <div className="bg-[#7b6ba7] rounded-2xl w-[40px] h-[40px] nav_sm:w-[50px] nav_sm:h-[50px] flex items-center justify-center">
+              <Image
+                src="question.svg"
+                width={24}
+                height={24}
+                alt="ques"
+                className="py-3 items-center justify-center"
+              />
             </div>
-            <div className="bg-[#7b6ba7] rounded-3xl p-4">
-              <Image src="trophy.svg" width={150} height={150} alt="ques" />
+            <div className="bg-[#7b6ba7] rounded-2xl w-[40px] h-[40px] nav_sm:w-[50px] nav_sm:h-[50px] flex items-center justify-center">
+              <Image
+                src="trophy.svg"
+                width={24}
+                height={24}
+                alt="ques"
+                className="py-3 items-center justify-center"
+              />
             </div>
-            <div className="bg-[#e9eaeb] rounded-3xl p-4 w-[180px]">
-              <Image src="refresh.svg" width={150} height={150} alt="ques" />
+            <div className="hidden nav_lg:flex cursor-not-allowed">
+              <div className="bg-[#e9eaeb] rounded-2xl w-[50px] h-[50px]  items-center justify-center flex">
+                <Image
+                  src="refresh.svg"
+                  width={24}
+                  height={24}
+                  alt="ques"
+                  className="py-3 items-center justify-center"
+                />
+              </div>
             </div>
           </div>
         </div>
