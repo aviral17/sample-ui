deca586ccfac532a2ab2496f27d152b5a17d7054
aviral <avi14320@gmail.com>
2023-11-23 01:03:02 +0530

    Wallet updated

--- app/(main)/(routes)/wallet/page.tsx
+++ app/(main)/(routes)/wallet/page.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
 const Wallet = () => (
-  <div className="flex h-full">
+  <div className="flex h-full wallet_font">
     <div className="w-1/3 p-4 flex flex-col  bg-[#f1f5f9] gap-20">
       <div className="self-center mb-10">
-        <h1 className="text-5xl font-medium text-[#160202a9]">My Wallet</h1>
+        <h1 className="text-5xl font-medium text-[#2c4e84]">My Wallet</h1>
       </div>
       <div className="flex justify-between">
         <div className="font-light text-4xl">Balance</div>
@@ -12,7 +12,7 @@ const Wallet = () => (
       </div>
     </div>
     <div className="w-2/3 p-4">
-      <div className="text-center text-5xl text-[#160202a9] mb-96">
+      <div className="text-center text-5xl text-[#630c0ca9] mb-96">
         Transactions
       </div>
       <div className="text-center">No Transactions yet ...</div>
